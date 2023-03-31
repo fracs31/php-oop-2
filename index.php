7f5ce5117d3363493460278fb9dd055c89ec43f8
fracs31 <pist.fracs95@gmail.com>
2023-03-31 15:54:20 +0200

    ADD fake cards

--- index.php
+++ index.php
@@ -4,10 +4,190 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ae23fa42a2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/style.css">
     <title>PHP OOP 2</title>
 </head>
 <body>
-    
+    <!-- Header -->
+    <header class="main-header">
+    </header>
+    <!-- Main -->
+    <main class="main-content">
+        <!-- Container -->
+        <div class="container">
+            <!-- Lista -->
+            <ul class="list">
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                            <!-- Immagine -->
+                            <img class="card__img" src="https://d2j6dbq0eux0bg.cloudfront.net/images/33879006/2965644321.jpg" alt="">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Crocchette
+                                <!-- Icona -->
+                                <span class="icon"><i class="fa-solid fa-dog"></i></span>
+                            </h2>
+                            <!-- Prezzo -->
+                            <h3 class="card__price">
+                                9.99€
+                            </h3>
+                            <!-- Tipologia -->
+                            <h4 class="card__type">
+                                Cibo
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
