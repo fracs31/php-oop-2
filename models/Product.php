e38a731eda4bbb4dd07744d0943ac13dbb9c4132
fracs31 <pist.fracs95@gmail.com>
2023-03-31 14:37:45 +0200

    ADD category attribute

--- models/Product.php
+++ models/Product.php
@@ -4,11 +4,13 @@ class Product {
     protected $img; //immagine
     protected $name; //nome
     protected $price; //prezzo
+    protected $category; //categoria
     //Metodo costruttore
-    public function __construct($_img, $_name, $_price) {
+    public function __construct($_img, $_name, $_price, $_category) {
         $this->img = $_img; //immagine
         $this->name = $_name; //nome
         $this->price = $_price; //prezzo
+        $this->category = $_category; //categoria
     }
     //Metodo che ritorna l'immagine del prodotto
     public function getImg() {
@@ -22,6 +24,10 @@ class Product {
     public function getPrice() {
         return $this->img; //restituisco il prezzo
     }
+    //Metodo che ritorna la categoria del prodotto
+    public function getCategory() {
+        return $this->category; //restituisco la categoria
+    }
     //Metodo per impostare l'immagine
     public function setImg($_img) {
         $this->img = $_img; //imposto l'immagine
@@ -33,6 +39,10 @@ class Product {
     //Metodo per impostare il prezzo
     public function setPrice($_price) {
         $this->price = $_price; //imposto il prezzo
+    }
+    //Metodo per impostare la categoria
+    public function setCategory($_category) {
+        $this->category = $_category; //imposto la categoria
     }
 }
 ?>
